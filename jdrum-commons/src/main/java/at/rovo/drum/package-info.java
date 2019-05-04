547f9da03cd81f36ab6346a668f02898ae682c31
Roman Vottner <rovo@gmx.at>
2019-05-04 15:40:43 +0200

    Added package-info files and updated to Java 11.0.3
    Added package-info files for further documentation of the package structure (TODO) and upgraded to Java 11.0.3 though this still does not fix the Javadoc bug once mvn site is performed

---
+++ jdrum-commons/src/main/java/at/rovo/drum/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Base classes for managing and interacting with the Disk-Repository with Update-Management (DRUM) system.
+ */
+package at.rovo.drum;
---
+++ jdrum-commons/src/main/java/at/rovo/drum/util/package-info.java
@@ -0,0 +1,1 @@
+package at.rovo.drum.util;
